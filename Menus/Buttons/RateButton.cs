fe66943b3e5b71ac7f103dc97ac612a24246a428
crschriever <crschriever@gmail.com>
2019-03-31 14:04:51 -0400

    Pre merge

--- Menus/Buttons/RateButton.cs
+++ Menus/Buttons/RateButton.cs
@@ -8,6 +8,13 @@ public class RateButton : MonoBehaviour
     public string APP_NAME;
     public void Rate()
     {
+#if UNITY_ANDROID
         Application.OpenURL("market://details?id=" + APP_NAME);
+
+#elif UNITY_IOS
+        Application.OpenURL("itms-apps://itunes.apple.com/app/" + APP_NAME);
+
+#endif
+
     }
 }
